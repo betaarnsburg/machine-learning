41ac4b893137516a8e89ced1c2aaa406d764c4a6
betaarnsburg <64945891+betaarnsburg@users.noreply.github.com>
2020-08-01 23:17:34 +0200

    Create IsolationForest.py

---
+++ IsolationForest.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.ensemble import IsolationForest
+
+df = pd.read_csv('train.csv')
+model=IsolationForest(n_estimators=50, max_samples='auto', contamination=float(0.1),max_features=1.0)
+model.fit(df[['target']])
+
+
+df['scores']=model.decision_function(df[['target']])
+df['anomaly']=model.predict(df[['target']])
+
+anomaly=df.loc[df['anomaly']==-1]
+anomaly_index=list(anomaly.index)
+print(anomaly)
